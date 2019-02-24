bb41f2a47a3e652ccfb0939e8ee9d6cd122ef010
JonathanStefanski <jonathanstefanski@gmail.com>
2019-02-24 20:44:23 +0100

    specify error on resolvers

--- Core-SPA/src/app/_resolvers/member-list.resolver.ts
+++ Core-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -15,7 +15,7 @@ export class MemberListResolver implements Resolve<User[]> {
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
         return this.userService.getUsers().pipe(
             catchError(error => {
-                this.alertify.error('Problem retrieving data');
+                this.alertify.error(error);
                 this.router.navigate(['/home']);
                 return of(null);
             })
